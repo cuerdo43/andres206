78f9f86e637706470b7afde54ce859421479f689
estudiantes <email@email.com>
2018-10-03 12:44:03 -0500

    ruby

---
+++ practica.rb
@@ -0,0 +1,40 @@
+#puts "ingrese un numero."
+    #numero = gets.chomp.to_i
+      
+ #if numero % 2 == 0
+ 
+        #puts "el numero #{numero} es par."
+
+ #elsif numero % 2 != 0
+ 
+  
+        # puts "el numero #{numero}es impar."
+
+          
+  #else 
+    
+        # puts "no se cumplio ninguna condicion"
+
+
+    #end
+
+
+ puts "escriba un nombre"
+
+    name = gets.chomp.to_s
+
+
+   case name 
+   
+    
+
+ when "jose" 
+
+        puts " #{name} es padre de jesus."
+
+ when "maria"
+      
+       puts "#{name}es la madre de jesus}"
+
+ else  puts "no se cumplio ninguna condicion"
+ end
